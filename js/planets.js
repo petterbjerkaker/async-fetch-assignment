481693d9891e10de73cbe96b8b0f91050758007f
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-03-14 12:27:53 +0100

    Add js file and functions to fetch and display data

---
+++ js/planets.js
@@ -0,0 +1,48 @@
+const fetchData = async ()=>{
+	try {
+		const response = await fetch ("https://swapi.dev/api/planets");
+		const data = await response.json();
+		const planetsList = document.querySelector(".planets-list");
+		data.results.forEach((element, index) => {
+			const li = document.createElement("li");
+
+			li.textContent = `${element.name} - Diameter: ${element.diameter}, Climate: ${element.climate}, Gravity: ${element.gravity}, Terrain: ${element.terrain}, Population: ${element.population}`;
+
+			const img = document.createElement("img");
+			if(index === 0) {
+				img.src = "/assets/images/planets/tatooine.jpg";
+			}if(index === 1){
+				img.src = "/assets/images/planets/alderaan.png";
+			}if(index === 2){
+				img.src = "/assets/images/planets/yavin-iv.webp";
+			}if(index === 3){
+				img.src = "/assets/images/planets/hoth.jpg";
+			}if(index === 4){
+				img.src = "/assets/images/planets/dagobah.jpg";
+			}if(index === 5){
+				img.src = "/assets/images/planets/bespin.webp";
+			}if(index === 6){
+				img.src = "/assets/images/planets/endor.jpg";
+			}if(index === 7){
+				img.src = "/assets/images/planets/naboo.webp";
+			}if(index === 8){
+				img.src = "/assets/images/planets/coruscant.webp";
+			}if(index === 9){
+				img.src = "/assets/images/planets/kamino.webp";
+			}
+			
+
+			li.appendChild(img);
+			planetsList.appendChild(li);
+			console.log(element);
+
+		
+		});
+
+	}catch(error) {
+
+	}
+};
+
+fetchData();
+
---
+++ js/species.js
@@ -0,0 +1,47 @@
+const fetchData = async ()=>{
+	try {
+		const response = await fetch ("https://swapi.dev/api/species");
+		const data = await response.json();
+		const speciesList = document.querySelector(".species-list");
+		data.results.forEach((element, index) => {
+			const li = document.createElement("li");
+
+			li.textContent = `${element.name} - Classification: ${element.classification}, Designation: ${element.designation}, Average height: ${element.average_height}, Skin colors: ${element.skin_colors}, Average lifespan: ${element.average_lifespan}, Language: ${element.language}`;
+
+			const img = document.createElement("img");
+			if(index === 0) {
+				img.src = "/assets/images/species/human.webp";
+			}if(index === 1){
+				img.src = "/assets/images/species/droid.webp";
+			}if(index === 2){
+				img.src = "/assets/images/species/wookie.webp";
+			}if(index === 3){
+				img.src = "/assets/images/species/rodian.webp";
+			}if(index === 4){
+				img.src = "/assets/images/species/hutt.webp";
+			}if(index === 5){
+				img.src = "/assets/images/species/yoda.webp";
+			}if(index === 6){
+				img.src = "/assets/images/species/trandoshan.webp";
+			}if(index === 7){
+				img.src = "/assets/images/species/mon_calamari.webp";
+			}if(index === 8){
+				img.src = "/assets/images/species/ewok.webp";
+			}if(index === 9){
+				img.src = "/assets/images/species/sullustan.webp";
+			}
+			
+
+			li.appendChild(img);
+			speciesList.appendChild(li);
+			console.log(element);
+
+		
+		});
+
+	}catch(error) {
+
+	}
+};
+
+fetchData();
