b480f04094295f3ade665f5226e92d6f48c81bb8
Petter F. Bjerkaker <143997069+petterbjerkaker@users.noreply.github.com>
2024-03-14 11:20:55 +0100

    Initialize js

---
+++ js/films.js
@@ -0,0 +1,40 @@
+const fetchData = async ()=>{
+	try {
+		const response = await fetch ("https://swapi.dev/api/films");
+		const data = await response.json();
+		const filmsList = document.querySelector(".films-list");
+		data.results.forEach((element, index) => {
+			const li = document.createElement("li");
+
+			li.textContent = `${element.title} - Episode: ${element.episode_id}, Opening credits: ${element.opening_crawl}, Director: ${element.director}, Producer: ${element.producer}, Release date: ${element.release_date}`;
+
+			const img = document.createElement("img");
+			if(index === 0) {
+				img.src = "/assets/images/films/a-new-hope.jpg";
+			}if(index === 1){
+				img.src = "/assets/images/films/empire-strikes-back.jpg";
+			}if(index === 2){
+				img.src = "/assets/images/films/return-of-the-jedi.jpg";
+			}if(index === 3){
+				img.src = "/assets/images/films/the-phantom-menace.jpg";
+			}if(index === 4){
+				img.src = "/assets/images/films/attack-of-the-clones.jpg";
+			}if(index === 5){
+				img.src = "/assets/images/films/revenge-of-the-sith.jpg";
+			}
+			
+
+			li.appendChild(img);
+			filmsList.appendChild(li);
+			console.log(element);
+
+		
+		});
+
+	}catch(error) {
+
+	}
+};
+
+fetchData();
+
